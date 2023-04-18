19555956c0a9ac14debf385186a99d4f206971ab
RamSreekar <jvramsreekar@gmail.com>
2023-04-18 18:30:29 +0530

    Add database configuration

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 
+const database = require("./configs/DatabaseConfig")
+
 app.get("", (request, response) => {
     response.status(200).send("<h1>Server!</h1>")
 })
 
-app.listen(9999, () => {
+app.listen(9000, () => {
     console.log("Server running!")
 })
