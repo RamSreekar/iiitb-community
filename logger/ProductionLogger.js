157962b0b9b600948325104046b3b60abd73d8f6
RamSreekar <jvramsreekar@gmail.com>
2023-04-23 03:10:52 +0530

    Rename logger files

---
+++ logger/ProductionLogger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const customFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp}  ${level}: ${message}`;
+});
+
+const developmentLogger = () => {
+    return createLogger({
+        level: 'info',
+        format: combine(timestamp({ format: "DD-MM-YYYY HH:MM:SS" }), customFormat),
+        transports: [
+            new transports.Console(),
+            new transports.File({ filename : "api-logs.log" })
+        ],
+    });
+}
+
+module.exports = developmentLogger;
--- logger/index.js
+++ logger/index.js
@@ -1,13 +1,14 @@
-const developmentLogger = require('./developmentLogger');
+const developmentLogger = require('./DevelopmentLogger');
+const productionLogger = require('./ProductionLogger');
 
 require("dotenv").config();
 
 
 let logger = null;
 
-// if(process.env.NODE_ENV == 'production') {
-//     logger = productionLogger();
-// }
+if(process.env.NODE_ENV == 'production') {
+    logger = productionLogger();
+}
 
 if(process.env.NODE_ENV === 'development') {
     logger = developmentLogger();
