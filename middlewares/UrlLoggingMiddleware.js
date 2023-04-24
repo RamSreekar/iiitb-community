1cd411c4208afef3f53af9960172716c066998e1
RamSreekar <jvramsreekar@gmail.com>
2023-04-24 18:17:44 +0530

    Add changes to UrlLoggingMiddleware

--- middlewares/UrlLoggingMiddleware.js
+++ middlewares/UrlLoggingMiddleware.js
@@ -3,6 +3,9 @@ const logger = require('../logger/index');
 let urlLogger;
 
 urlLogger = (req, res, next) => {
+    if(req.originalUrl === '/favico.ico') {
+        next();
+    }
     logger.info(req.method + " " + req.originalUrl);
     next();
 }
