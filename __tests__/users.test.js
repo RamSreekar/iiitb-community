a3e29eb3c4c1f318c66ab77ecba2a03a87b3ed19
Ram Sreekar <jvramsreekar@gmail.com>
2023-04-24 20:51:26 +0530

    Add tests for registerUser

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -1,100 +1,135 @@
 const userController = require("../controllers/UserController");
-const User = require('../models/UserModel'); 
+const User = require("../models/UserModel");
 
-jest.mock('../models/UserModel');
+jest.mock("../models/UserModel");
 
 const mockRequest = {
-    params : {
-        userId : ""
-    }
-}
+  params: {
+    userId: "",
+  },
+};
 
 const mockResponse = () => {
-    const res = {};
-    res.status = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockReturnValue(res);
-    return res;
-}
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
 
 const mockErrorResponse = {
-    "error" : "Error",
-    "message" : "[object Object]"
-}
+  error: "Error",
+  message: "[object Object]",
+};
 
 const mockUserObject = {
-    _id : "",
-    email : "",
-    userType : "",
-    validGroups : []
-}
-
+  _id: "",
+  email: "",
+  userType: "",
+  validGroups: [],
+};
 
 describe("User API", () => {
-    test("getAllUsers() : should return status 200 and list of all users", async () => {
-        const request = mockRequest;
-        const response = mockResponse();
-    
-        User.find.mockImplementationOnce(() => []);
-    
-        await userController.getAllUsers(request, response);
-    
-        expect(response.status).toHaveBeenCalledWith(200);
-        expect(response.json).toHaveBeenCalledTimes(1);
-        expect(response.json).toHaveBeenCalledWith([]);
-    }); 
+  test("getAllUsers() : should return status 200 and list of all users", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
 
-    test("getAllUsers() : should return status 500 and error description", async () => {
-        const request = mockRequest;
-        const response = mockResponse();
-    
-        User.find.mockImplementationOnce(() => {
-            throw new Error(mockErrorResponse);
-        });
-    
-        await userController.getAllUsers(request, response);
-    
-        expect(response.status).toHaveBeenCalledWith(500);
-        expect(response.json).toHaveBeenCalledTimes(1);
-        expect(response.json).toHaveBeenCalledWith(mockErrorResponse); 
+    User.find.mockImplementationOnce(() => []);
+
+    await userController.getAllUsers(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith([]);
+  });
+
+  test("getAllUsers() : should return status 500 and error description", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
+
+    User.find.mockImplementationOnce(() => {
+      throw new Error(mockErrorResponse);
     });
 
-    test("getUserById() : should return status 200 and a user with the given id", async () => {
-        const request = mockRequest;
-        const response = mockResponse();
-    
-        User.findById.mockImplementationOnce(() => mockUserObject);
-    
-        await userController.getUserById(request, response);
-    
-        expect(response.status).toHaveBeenCalledWith(200);
-        expect(response.json).toHaveBeenCalledTimes(1);
-        expect(response.json).toHaveBeenCalledWith(mockUserObject);
+    await userController.getAllUsers(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith(mockErrorResponse);
+  });
+
+  test("getUserById() : should return status 200 and a user with the given id", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
+
+    User.findById.mockImplementationOnce(() => mockUserObject);
+
+    await userController.getUserById(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith(mockUserObject);
+  });
+
+  test("getUserById() : should return status 404 and user as null", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
+
+    User.findById.mockImplementationOnce(() => null);
+
+    await userController.getUserById(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(404);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith({
+      message: "User doesn't exist with given id!",
+    });
+  });
+
+  test("getUserById() : should return status 500 and error description", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
+
+    User.findById.mockImplementationOnce(() => {
+      throw new Error(mockErrorResponse);
     });
 
-    test("getUserById() : should return status 404 and user as null", async () => {
-        const request = mockRequest;
-        const response = mockResponse();
-    
-        User.findById.mockImplementationOnce(() => null);
-    
-        await userController.getUserById(request, response);
-    
-        expect(response.status).toHaveBeenCalledWith(404);
-        expect(response.json).toHaveBeenCalledTimes(1);
-        expect(response.json).toHaveBeenCalledWith({"message": "User doesn't exist with given id!"});
+    await userController.getUserById(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith(mockErrorResponse);
+  });
+
+  test("registerUser() : should return status 200 and post an announcement", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
+
+    const user = new User(request.body);
+    user.save.mockImplementationOnce();
+
+    await userController.registerUser(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith({
+      message: "User created!",
+    });
+  });
+
+  test("registerUser() : should return status 500 and error description", async () => {
+    const request = mockRequest;
+    const response = mockResponse();
+
+    const user = new User(request.body);
+
+    user.save.mockImplementationOnce(() => {
+      throw new Error(mockErrorResponse);
     });
 
-    test("getUserById() : should return status 500 and error description", async () => {
-        const request = mockRequest;
-        const response = mockResponse();
-    
-        User.findById.mockImplementationOnce(() => {throw new Error(mockErrorResponse)});
-    
-        await userController.getUserById(request, response);
-    
-        expect(response.status).toHaveBeenCalledWith(500);
-        expect(response.json).toHaveBeenCalledTimes(1);
-        expect(response.json).toHaveBeenCalledWith(mockErrorResponse); 
-    });
+    await userController.registerUser(request, response);
+
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledTimes(1);
+    expect(response.json).toHaveBeenCalledWith(mockErrorResponse);
+  });
 });
-
